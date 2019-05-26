3e9344634bb015d2aeb99f263095443b096417b9
itea1001 <51052416+itea1001@users.noreply.github.com>
2019-05-26 22:04:13 +0800

    Create Hello World.cpp

---
+++ Hello World.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define rint register int
+using namespace std;
+typedef pair<int,int> pii;
+typedef long long ll;
+
+
+
+int main(){
+    printf("Hello World!"\n);
+    return 0;
+}
